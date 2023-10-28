0585b01a0d6a7c60e4c08eb8ee323089778aef48
SannetMutsambwa <sannetmutsambwa@yahoo.com>
2023-10-28 03:45:14 +0200

    Static_libraries

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,4 +1,5 @@
 #include "main.h"
+void_puts(char *str)
 #include<stdio.h>
 /**
 * main - Entry point
@@ -7,6 +8,6 @@
 int main(void)
 
 {
-	_puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
