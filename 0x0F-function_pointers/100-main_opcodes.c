1a2e945005d03011918138ce9ed9d217c34b3f64
SannetMutsambwa <sannetmutsambwa@yahoo.com>
2023-11-08 19:12:42 +0200

    functions_pointers

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -28,7 +28,7 @@ if (bytes < 0)
 printf("Error\n");
 exit(2);
 }
-arr = (char *)main;
+arr = (char *) main;
 for (i = 0; i < bytes; i++)
 {
 if (i == bytes - 1)
@@ -37,6 +37,6 @@ printf("%02hhx\n", arr[i]);
 break;
 }
 printf("%02hhx ", arr[i]);
-} 
-return(0);
+}
+return (0);
 }
--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -12,7 +12,9 @@
 int (*get_op_func(char *s))(int, int)
 
 {
+
 	op_t ops[] = {
+
 		{"+", op_add},
 		{"-", op_sub},
 		{"*", op_mul},
@@ -22,6 +24,7 @@ int (*get_op_func(char *s))(int, int)
 	};
 
 	int i = 0;
+
 	while (ops[i].op != NULL && *(ops[i].op) != *s)
 		i++;
 	return (ops[i].f);
