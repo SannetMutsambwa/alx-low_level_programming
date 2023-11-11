0a26dd21eb69b26be8996374885f6664bb04ab4d
SannetMutsambwa <sannetmutsambwa@yahoo.com>
2023-11-11 04:59:09 +0200

    variadic_functions

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,8 +9,8 @@
 */
 
 int sum_them_all(const unsigned int n, ...)
-
 {
+
 	va_list ap;
 
 	unsigned int i, sum = 0;
@@ -20,5 +20,6 @@ int sum_them_all(const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 		sum += va_arg(ap, int);
 	va_end(ap);
+
 	return (sum);
 }
