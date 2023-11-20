8fa433987515e40846e86bdbf5b28e3c679439e3
SannetMutsambwa <sannetmutsambwa@yahoo.com>
2023-11-20 15:45:16 +0200

    links

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -16,7 +17,9 @@ char *str;
 unsigned int len;
 struct list_s *next;
 } list_t;
-size_t print_list(count list_t *h);
+int _putchar(char c);
+int _strlen(char *s);
+size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
