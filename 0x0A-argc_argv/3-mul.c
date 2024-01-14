91809825ccbfcb208dd52670847cbb2a1f94874c
SannetMutsambwa <sannetmutsambwa@yahoo.com>
2024-01-14 19:32:04 +0200

    Argc_Argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -40,7 +40,9 @@ f = 0;
 }
 i++;
 }
+
 if (f == 0)
+
 return (0);
 return (n);
 }
@@ -60,9 +62,12 @@ if (argc < 3 || argc > 3)
 printf("Error\n");
 return (1);
 }
+
 num1 = _atoi(argv[1]);
+
 num2 = _atoi(argv[2]);
-result = num1 * num2;
+
+result = num1 *num2;
 printf("%d\n", result);
 return (0);
 }
