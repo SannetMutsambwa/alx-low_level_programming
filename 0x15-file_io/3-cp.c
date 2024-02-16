841ed7bc0c5cc46a4bb9147089cc805ff33fbb95
SannetMutsambwa <sannetmutsambwa@yahoo.com>
2024-02-16 21:47:37 +0200

    file_IO

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,14 +8,13 @@ void close_file(int fd);
 * @file: The name of the file buffer is storing chars for.
 * Return: A pointer to the newly-allocated buffer.
 */
-char *creates_buffer(char *file)
+char *create_buffer(char *file)
 {
 char *buffer;
 buffer = malloc(sizeof(char) * 1024);
 if (buffer == NULL)
 {
-dprintf(STDERR_FILENO,
-"Error: Can't write to %s\n", file);
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
 exit(99);
 }
 return (buffer);
